88b454e3e8b7de05d41e3e11d344243427e807c8
ahmed serag <ahmed serag>
2017-07-10 13:11:40 +0200

    first upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# neighborhood-map
+#Neighborhood Map
+- this is an application shows my map and my favourite marks on it
+
+# run instructions
+- open index.html where you can see my map loaded and marks on it
+- click on each mark to see the place name and how many people in it now, from foursquare API
+- there is a search function to search for a specific function in the list
---
+++ assets/js/demo.js
@@ -0,0 +1,98 @@
+var mapMarkers=[];
+var infowindows = [];
+function init(){
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 31.163827, lng: 29.867720},
+        zoom: 18
+    });
+
+    var viewmodel = ViewModel(map);
+   
+    ko.applyBindings(viewmodel);
+}
+
+function ViewModel(map){
+    this.searchQuery = ko.observable('');
+
+    this.Markers = ko.observableArray([
+        {title: "home ", lat: 31.165084, lng: 29.868484, index: 0},
+        {title: "school ", lat: 31.163827, lng: 29.867720, index: 1},
+        {title: "restaurant ", lat: 31.163831, lng: 29.867816, index: 2},
+        {title: "hospital ", lat: 31.163147, lng: 29.867435, index: 3},
+        {title: "shop ", lat: 31.163992, lng: 29.867827, index: 4}
+    ]);
+
+    this.ActiveMarkers = ko.computed(function() {
+
+        var result = $.grep(this.Markers(), function(arr) {
+            var res = arr.title.indexOf(this.searchQuery());
+            return (res > -1);
+        });
+        
+        ko.utils.arrayForEach(mapMarkers, function(marker){
+            marker.setMap(null);
+        });
+        
+        mapMarkers = [];
+        
+        this.clickItem = function(item){
+            mapMarkers[item.index].setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() {
+                mapMarkers[item.index].setAnimation(null);
+            }, 1200);
+            infowindows[item.index].open(map, mapMarkers[item.index]);
+        }
+        ko.utils.arrayForEach(result, function(marker) {    
+            // Create a latLng literal object.
+            var singleLatLng = {lat: marker.lat, lng: marker.lng};	
+            
+            var singleMarker = new google.maps.Marker({
+            position:singleLatLng,
+            map: map,
+            title: marker.title
+            });
+            mapMarkers.push(singleMarker);
+            
+            foursquareUrl = 'https://api.foursquare.com/v2/venues/search' +
+            '?client_id=QP1LCTQNC4OCBGLAK31KTGKP5WPYG1I3OJKTS1UU0IY1RNB0' +
+            '&client_secret=J1NHLR3FTFKH24CSI2YIPCQYW4FBZ4VJVT4YNJ4LGT0MBBGB' +
+            '&v=20130815' +
+            '&limit=1' +
+            '&ll=' + marker.lat + ',' + marker.lng;
+            var infoContent = 'bla bla';
+            $.getJSON(foursquareUrl, function(data) {         
+                if(data.response.venues){
+                    result = data.response.venues[0];
+                    infoContent = "<h3>"+result.name+"</h3>"+
+                        "<p> there are "+result.hereNow.count+" here now!";
+                    initinfoWindow(map, singleMarker, infoContent);
+                   /*
+                    var infowindow = new google.maps.InfoWindow({
+                        content: infoContent
+                    });
+                    singleMarker.addListener('click', function() {
+                        infowindow.open(map, singleMarker);
+                    });
+                    */
+                } 
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                 infoContent = "Sorry Can't connect to the API right now!!";
+                 initinfoWindow(map, singleMarker, infoContent);
+            });    
+	    });
+        return result;
+    });
+}
+
+function initinfoWindow(map, marker, content){
+    var infowindow = new google.maps.InfoWindow({
+        content: content
+    });
+    infowindows.push(infowindow);
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+}
+
+
